0c60ac83282405bd66301abb63d2e86bff0fe6f6
Elenildo <elenildoms@gmail.com>
2024-10-18 16:36:59 -0300

    Code refactoring

--- src/main/java/com/youtan/sistema/services/UserService.java
+++ src/main/java/com/youtan/sistema/services/UserService.java
@@ -6,7 +6,7 @@ import com.youtan.sistema.models.Role;
 import com.youtan.sistema.models.User;
 import com.youtan.sistema.repositories.RoleRepository;
 import com.youtan.sistema.repositories.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -14,13 +14,11 @@ import java.util.List;
 
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
 
-    @Autowired
-    private UserRepository userRepository;
-
-    @Autowired
-    private RoleRepository roleRepository;
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
 
     public User create(User user) {
         var usr = userRepository.findByUsername(user.getUsername());
